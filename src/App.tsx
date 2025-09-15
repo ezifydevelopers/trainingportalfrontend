02e1b1e22a82e3d1e0952f50eb4c6ac29ad7f285
ezifyDev <muhammadbilal@ezaccounts.ca>
2025-09-15 23:08:52 +0500

    Staging remove unnecessary comments

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ import Chat from "./pages/Chat";
 
 const queryClient = new QueryClient();
 
-// Dashboard redirect component to redirect to the appropriate page based on the user's role
+// Dashboard redirect component 
 const DashboardRedirect = () => {
   const { user } = useAuth();
   
